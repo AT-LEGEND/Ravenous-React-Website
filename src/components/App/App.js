941f3d7d6c3ca30fc3f171704fd74e0e03bacd6e
AT-LEGEND <atlegend0@gmail.com>
2020-12-01 15:35:00 +0530

    part two completed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,28 @@ import "./App.css";
 import React from "react";
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
-function App() {
-	return (
-		<div class='App'>
-			<h1>ravenous</h1>
-			<SearchBar />
-			<BusinessList />
-		</div>
-	);
+const business = {
+	imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
+	name: "Margherita Pizzeria",
+	address: "1010 Paddington Way",
+	city: "Flavortown",
+	state: "NY",
+	zipCode: "10101",
+	category: "Italian",
+	rating: 4.5,
+	reviewCount: 90,
+};
+const businesses = [business, business, business, business, business, business];
+class App extends React.Component {
+	render() {
+		return (
+			<div className='App'>
+				<h1>ravenous</h1>
+				<SearchBar />
+				<BusinessList businesses={businesses} />
+			</div>
+		);
+	}
 }
 
 export default App;
